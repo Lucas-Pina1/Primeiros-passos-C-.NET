3b073184359408912e0a1909503f62f1e00614fb
Lucas Pina <ti.lucaspina@gmail.com>
2021-10-10 20:25:33 -0300

    continuação

---
+++ Aluno.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Primeiros-passos-C-.NET
+{
+    public struct Aluno
+    {
+        public string Nome { get; set; }
+
+        public decimal Nota { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ namespace revisao
   {
     static void Main(string[] args)
     {
+      Aluno[] alunos = new Aluno[5];
+      var indeceAluno = 0;
       string opcaoUsuario = ObterOpcaoUsuario();
 
       while (opcaoUsuario.ToUpper() != "X")
@@ -13,10 +15,33 @@ namespace revisao
         switch (opcaoUsuario)
         {
           case "1":
+            Console.WriteLine("Informe o nome do aluno:");
+            var aluno = new Aluno();
+            aluno.Nome = Console.ReadLine();
+
+            Console.WriteLine("Informe a nota do aluno:");
+
+            if (decimal.TryParse(Console.ReadLine(), out decimal nota)) 
+            {
+              aluno.Nota = nota;
+            } 
+            else 
+            {
+              throw new ArgumentException("Valor da nota deve ser decimal");
+            } 
+
+            alunos[indeceAluno] = aluno;
+            indeceAluno++;
 
             break;
           case "2":
-
+            foreach (var item in alunos)
+            { 
+              if (!string.IsNullOrEmpty(item.Nome)) 
+              {
+                 Console.WriteLine($"ALUNO: {item.Nome} - NOTA: {item.Nota}");
+              }
+            }
             break;
           case "3":
 
@@ -30,6 +55,7 @@ namespace revisao
 
     private static string ObterOpcaoUsuario()
     {
+      Console.WriteLine();
       Console.WriteLine("informe a opção desejada");
       Console.WriteLine("1- Inserir novo aluno");
       Console.WriteLine("2- Listar alunos");
